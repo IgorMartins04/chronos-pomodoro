53340cbd6109f99adca675a768e46da186f16395
Igor Martins <igormartins1563@gmail.com>
2025-10-30 20:00:12 -0300

    create color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/theme.css";
+import "./styles/global.css";
+import "./components/Heading";
+import { Heading } from "./components/Heading";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  console.log("oi");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <Heading>Testando 1</Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sint
+        deserunt fugit minus provident repellat at error deleniti accusantium
+        culpa non quasi, eligendi minima expedita fugiat modi ullam velit
+        corrupti?
       </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
