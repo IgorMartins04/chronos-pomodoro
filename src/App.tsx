07d604a2b43ad45ef4ba327802f316ff85577822
Igor Martins <igormartins1563@gmail.com>
2025-11-03 20:13:16 -0300

    Creating elements for the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import "./components/Heading";
-import { Heading } from "./components/Heading";
+
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
+import { DefaultInpunt } from "./components/DefaultInput";
 
 export function App() {
-  console.log("oi");
-
   return (
-    <div>
-      <Heading>Testando 1</Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sint
-        deserunt fugit minus provident repellat at error deleniti accusantium
-        culpa non quasi, eligendi minima expedita fugiat modi ullam velit
-        corrupti?
-      </p>
-    </div>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form action="" className="fomr">
+          <div className="formRow">
+            <DefaultInpunt
+              labelText="teste"
+              id="meuInput"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 </p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
+    </>
   );
 }
