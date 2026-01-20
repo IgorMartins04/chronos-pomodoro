bccdaf9b231dd37645687c4c6793a9fc83d5b477
Igor Martins <igormartins1563@gmail.com>
2026-01-19 23:42:59 -0300

    Funções de tempo e ciclos

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,64 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInpunt } from "../DefaultInput";
+import type { TaskModel } from "../../models/TaskModels";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { useRef } from "react";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!taskNameInput.current) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining: secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
   return (
-    <form action="" className="form">
+    <form onSubmit={handleCreateNewTask} action="" className="form">
       <div className="formRow">
         <DefaultInpunt
           labelText="task"
           id="meuInput"
           type="text"
           placeholder="Digite algo"
+          ref={taskNameInput}
         />
       </div>
 
@@ -19,10 +67,11 @@ export function MainForm() {
         <p>O proximo intervalo é de 25min</p>
       </div>
 
-      <div className="formRow">
-        <Cycles></Cycles>
-      </div>
-
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles></Cycles>
+        </div>
+      )}
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} />
       </div>
