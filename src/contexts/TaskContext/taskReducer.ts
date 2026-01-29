2ed17268fc75a233fc69f89510b908705ec36d47
Igor Martins <igormartins1563@gmail.com>
2026-01-29 19:00:54 -0300

    Logica do contador

--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -22,16 +22,6 @@ export function taskReducer(
       };
     }
     case TaskActionTypes.INTERRUPT_TASK: {
-      //     ...prevState,
-      //     activeTask: null,
-      //     secondsRemaining: 0,
-      //     formattedSecondsRemaining: "00:00",
-      //     tasks: prevState.tasks.map((task) => {
-      //       if (prevState.activeTask && prevState.activeTask.id === task.id) {
-      //         return { ...task, interruptDate: Date.now() };
-      //       }
-      //       return task;
-      //     }),
       return {
         ...state,
         activeTask: null,
@@ -43,6 +33,29 @@ export function taskReducer(
           }
           return task;
         }),
+      };
+    }
+    case TaskActionTypes.COMPLETE_TASK: {
+      return {
+        ...state,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: "00:00",
+        tasks: state.tasks.map((task) => {
+          if (state.activeTask && state.activeTask.id === task.id) {
+            return { ...task, completeDate: Date.now() };
+          }
+          return task;
+        }),
+      };
+    }
+    case TaskActionTypes.COUNT_DOWN: {
+      return {
+        ...state,
+        secondsRemaining: action.payload.secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(
+          action.payload.secondsRemaining,
+        ),
       };
     }
     case TaskActionTypes.RESET_STATE: {
